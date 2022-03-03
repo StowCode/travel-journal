8dc1b338cd82a4985a071cab0e1173b7b66ea174
Mitchell Stowman <92927307+StowCode@users.noreply.github.com>
2022-03-02 19:02:36 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from "./Header.js";
+import Card from "./Card.js";
+import Data from "./data.js";
 
-function App() {
+export default function App() {
+
+  const cards = Data.map(item => {
+    return (
+        <Card
+          key= {item.id}
+          title= {item.title}
+          location= {item.location}
+          googleMapsUrl= {item.googleMapsUrl}
+          startDate= {item.startDate}
+          endDate= {item.endDate}
+          description={item.description}
+          imageUrl={item.imageUrl}
+          />
+      )
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      {cards}
     </div>
   );
 }
 
-export default App;
+ 
